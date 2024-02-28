079ecf6c5cbca9c01cd655723aedc9f89b3724d2
Neer-Shrestha <shrestha.neer@hotmail.com>
2024-02-28 23:09:41 +0545

    feat: add clear cart and update get_products

--- includes/products.php
+++ includes/products.php
@@ -44,13 +44,28 @@ function get_product_by_id($product_id)
     global $con;
 
     $stmt = $con->prepare("SELECT * FROM products WHERE product_id = ?");
-    $stmt->bind_param('i', $product_id);
+    $stmt->bind_param('s', $product_id);
     if ($stmt->execute()) :
         $result = $stmt->get_result();
-        $data = $result->fetch_all(MYSQLI_ASSOC);
+        $data = $result->fetch_array(MYSQLI_ASSOC);
     else :
         $data['error'] = 'Something went wrong while fetching product.';
     endif;
 
     return $data;
+}
+
+function clear_cart()
+{
+    global $con;
+
+    $user_id = get_user_id();
+
+    if ($user_id == null) :
+        header("Location: index.php");
+    endif;
+
+    $stmt = $con->prepare("DELETE FROM cart WHERE user_id = ?");
+    $stmt->bind_param('s', $user_id);
+    $stmt->execute();
 }

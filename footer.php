da58a03fea8fa5d4a95ee6a1291a69975e9f05aa
Neer-Shrestha <shrestha.neer@hotmail.com>
2024-02-28 21:24:18 +0545

    feat: link styles and scripts

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./assets/js/script.js"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -13,6 +13,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Closet</title>
+    <link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap.min.css">
 </head>
 
 <body>

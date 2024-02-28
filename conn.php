1b7d6f92b60698cd33923e677fe42c9ff13a7ec9
prabhatgrg <prabhatgurung34@gmail.com>
2024-02-28 15:28:40 +0545

    feat: Added connection config

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+define('DB_NAME', 'rentease');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+function open_con()
+{
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    //Check Connection
+    if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+function close_con($conn)
+{
+    $conn->close();
+}

77c9249d025db6efe2dbf90d22e69c687589f746
prabhatgrg <prabhatgurung34@gmail.com>
2024-02-29 00:56:11 +0545

    feat: Login logout functionality added

--- index.php
+++ index.php
@@ -4,53 +4,57 @@ require 'header.php'; ?>
 <section class="cart py-3">
     <div class="container">
         <div class="row">
-            <div class="col-12 text-end">
-                <a href="clear_cart.php" class="btn btn-danger">Empty Cart</a>
-                <a href="orders.php" class="me-2 btn btn-danger">View my orders</a>
-            </div>
-            <div class="col-12">
-                <?php
-                $cart_products = get_cart_products();
-                if (!isset($cart_products['error'])) : ?>
+            <?php if (is_login()) : ?>
+                <div class="col-12 text-end">
+                    <a href="clear_cart.php" class="btn btn-danger">Empty Cart</a>
+                    <a href="orders.php" class="me-2 btn btn-danger">View my orders</a>
+                </div>
+                <div class="col-12">
+                    <?php
+                    $cart_products = get_cart_products();
+                    if (!isset($cart_products['error'])) : ?>
 
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Code</th>
-                                <th>Quantity</th>
-                                <th>Unit price</th>
-                                <th>Price</th>
-                                <th>Remove</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-
-                            <?php foreach ($cart_products as $cart_product) :
-                                $product = get_product_by_id($cart_product['product_id']);
-                            ?>
-
+                        <table class="table table-hover">
+                            <thead>
                                 <tr>
-                                    <td><img style="width: 4rem; aspect-ratio: 1; object-fit-cover;" class="rounded-circle me-2" src="./uploads/<?php echo $product['product_image']; ?>" alt="<?php echo $product['product_title'] ?>"> <?php echo $product['product_title'] ?></td>
-                                    <td><?php echo $product['product_code'] ?></td>
-                                    <td><?php echo $cart_product['cart_qty'] ?></td>
-                                    <td><?php echo $cart_product['cart_price'] ?></td>
-                                    <td><?php echo $cart_product['total_price'] ?></td>
-                                    <td><a href="remove_cart.php?cart_id=<?php echo $cart_product['cart_id'] ?>" class="btn-close" aria-label="Close"><span class="d-none">close</span></a></td>
+                                    <th>Name</th>
+                                    <th>Code</th>
+                                    <th>Quantity</th>
+                                    <th>Unit price</th>
+                                    <th>Price</th>
+                                    <th>Remove</th>
                                 </tr>
+                            </thead>
 
-                            <?php endforeach; ?>
+                            <tbody>
 
-                        </tbody>
+                                <?php foreach ($cart_products as $cart_product) :
+                                    $product = get_product_by_id($cart_product['product_id']);
+                                ?>
 
-                    </table>
+                                    <tr>
+                                        <td><img style="width: 4rem; aspect-ratio: 1; object-fit-cover;" class="rounded-circle me-2" src="./uploads/<?php echo $product['product_image']; ?>" alt="<?php echo $product['product_title'] ?>"> <?php echo $product['product_title'] ?></td>
+                                        <td><?php echo $product['product_code'] ?></td>
+                                        <td><?php echo $cart_product['cart_qty'] ?></td>
+                                        <td><?php echo $cart_product['cart_price'] ?></td>
+                                        <td><?php echo $cart_product['total_price'] ?></td>
+                                        <td><a href="remove_cart.php?cart_id=<?php echo $cart_product['cart_id'] ?>" class="btn-close" aria-label="Close"><span class="d-none">close</span></a></td>
+                                    </tr>
 
-                <?php else : ?>
-                    <p class="text-center">Your cart is empty.</p>
-                <?php endif;
-                ?>
-            </div>
+                                <?php endforeach; ?>
+
+                            </tbody>
+
+                        </table>
+
+                    <?php else : ?>
+                        <p class="text-center">Your cart is empty.</p>
+                    <?php endif;
+                    ?>
+                </div>
+            <?php else :?>
+                <p class="text-center">Login to shop</p>
+            <?php endif; ?>
         </div>
     </div>
 </section>
